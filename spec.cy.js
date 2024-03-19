7a5e9b1dc04e88deb9b7163df335ac0d3f9b973e
JoroveanuAlexandra <127882929+JoroveanuAlexandra@users.noreply.github.com>
2024-03-20 00:21:04 +0200

    Modifying  the tests

--- spec.cy.js
+++ spec.cy.js
@@ -1,28 +1,36 @@
 
 describe('Routes',()=>{
-  it('Passes',()=>{
-  cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net')
-  cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/signup')
+  it('Visit the sign-up page by clicking',()=>{
+  cy.viewport(1920, 1080)
+  cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/signup')})
+  it('Visit the login page by clicking',()=>{
   cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/login')
+  })
+  it('Visit the profile page by clicking',()=>{
   cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/profile')
-  cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/department')
-  cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/project')
+  })
+  it('Visit the administrator departments page by clicking',()=>{
+  cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/department')})
+  it('Visit the project manager page by clicking',()=>{
+  cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/project')})
+  it('Visit the admin organization page by clicking',()=>{
   cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/admin')
 })
 })
 
 
-describe('Verify buttons',()=>{
+describe('Verify buttons on the login and sign-up page',()=>{
   it("Verify the functionality of the buttons",()=>{
   cy.viewport(1920, 1080)
   cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/login')
-  cy.get('button[type="submit"]').click();
-  cy.get('button').contains('Register').click();
-  cy.get('button').contains('Login').click();
-  cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/signup')
-  cy.get('button').contains('Go to Login Page').click();
-  cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/profile')
-  cy.get('button').contains('Add Skills').click();
+  cy.request('POST', '/login', {
+    email: 'test@email.com',
+    pass: 'testPass'
+    });
+  cy.xpath("//button[normalize-space()='Login']").click();
+  cy.xpath("//button[normalize-space()='Register']").click()
+  cy.xpath("//button[normalize-space()='Go to Login Page']").click()
+
 })
 })
 
@@ -35,20 +43,20 @@ describe('Login-Succeed', () => {
       email: 'test@email.com',
       pass: 'testPass'
       });
-    cy.contains('Login').click();
+    cy.xpath("//button[normalize-space()='Login']")
   })
 })
 
 
 describe('Login-Failed', () => {
-  it('Verify that Login functionality works with valid credentials.', () => {
+  it('Verify that Login functionality works with invalid credentials.', () => {
     cy.viewport(1920, 1080)
     cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/login')
     cy.request('POST', '/login', {
     email: 'test',
     pass: 'test'
     });
-    cy.contains('Login').click();
+    cy.xpath("//button[normalize-space()='Login']")
   })
 })
 
@@ -57,14 +65,16 @@ describe('Register-Succeed', () => {
     it('Verify that Login functionality works with valid credentials.', () => {
       cy.viewport(1920, 1080)
       cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/signup')
-      cy.get('[placeholder="Name"]').type("Nume Prenume")
+      cy.get('placeholder="Name"').type("Nume Prenume");
       cy.request('POST', '/login', {
       email: 'test@email.com',
       pass: 'testPass'
       });
       cy.get('[placeholder="orgName"]').type("Organization")
       cy.get('[placeholder="orgAdrress"]').type("Country, City, Street, Number")
-      cy.contains('Login').click();
+      cy.xpath("//button[normalize-space()='Register']").click()
+      cy.xpath("//button[normalize-space()='Go to Login Page']").click()
+
     })
   })
 
@@ -80,73 +90,90 @@ describe('Register-Failed', () => {
       });
       cy.get('[placeholder="orgName"]').type("123")
       cy.get('[placeholder="orgAdrress"]').type("Country")
-      cy.contains('Login').click();
+      cy.xpath("//button[normalize-space()='Register']").click()
+      cy.xpath("//button[normalize-space()='Go to Login Page']").click()
     })
   })
 
-
-describe('Display Employees', () => {
+describe('Employee',()=>{
   beforeEach(() => {
+  cy.viewport(1920, 1080)
+        cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/profile');
+      });
+      it('Toggle Current Projects',()=>{
+        cy.xpath("//button[normalize-space()='Toggle Current Projects']").click()
+        cy.get('h2').contains('Current Projects').should('be.visible')
+
+      })
+      it('Toggle Past Projects',()=>{
+        cy.xpath("//button[normalize-space()='Toggle Past Projects']").click()
+        cy.get('h2').contains('Past Projects').should('be.visible')
+      })
+      it('Add Skills',()=>{
+        cy.xpath("//button[normalize-space()='Add Skills']").click()
+        cy.get('h2').contains('Add Skills').should('be.visible')
+      })    
+})
+describe('Department Manager', () => {
+  beforeEach(() => {
+  cy.viewport(1920, 1080)
     cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/department');
   });
-
+  it('Verify the notification',()=>{
+    cy.get('span').contains('Notifications: ').should('exist')
+  })
   it('Should verify the display of department members', () => {
-    cy.contains('Department Members').click();
+    cy.xpath("//button[normalize-space()='Department Members']").click()
     cy.get('h3').contains('Department Members').should('be.visible');
   });
-
+  it('Should verify the Statistic Show on the screen',()=>{
+    cy.xpath("//button[normalize-space()='Show Statistics']").click()
+   })
   it('Should verify the display of unassigned department members', () => {
-    cy.contains('Unassigned Members').click();
+    cy.xpath("//button[normalize-space()='Unassigned Members']").click()
     cy.get('h3').contains('Unassigned Department Employees').should('be.visible');
   });
-
+  it('Should verify the display of the assignment proposal', () => {
+    cy.xpath("//button[normalize-space()='Assignment Proposals']").click()
+    cy.get('h3').contains('Assignment Proposals').should('be.visible');
+  });
   it('Should verify the display of department skills', () => {
-    cy.contains('Manage Skills').click();
+    cy.xpath("//button[normalize-space()='Manage Skills']").click()
     cy.get('h3').contains('Department Skills').should('be.visible');
   });
 });
 
 
-describe('Administration Page', () => {
+describe('Organization Admin', () => {
+  beforeEach(() => {
+      cy.viewport(1920, 1080)
+      cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/admin');
+      });
   it('Redirect to employee sign-up page when "Employee sign-up link" is clicked', () => {
-    cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/admin');
-    if(cy.contains('Employee sign-up link').click())
-      cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/employee-register');
-  });
+    if(cy.get('a').contains('Employee sign-up link'))
+     cy.xpath("//a[normalize-space()='Employee sign-up link']").click()
+  })
+  it('Verify if the buttons Show Employees button present the data',() => {
+    cy.xpath("//button[normalize-space()='Show Employees']").click()
+    cy.get('h2').contains('Employees').should('be.visible');
+  })
+  it('Should verify if the click on the Show Team Roles button present the data ',()=>{
+    cy.xpath("//button[normalize-space()='Show Team Roles']").click();
+    cy.get('h2').contains('Team Roles').should('be.visible');
+  })
+  it('Should verify if the click on the Manage Departments button present the data ',()=>{
+    cy.xpath("//button[normalize-space()='Manage Departments']").click();
+    cy.get('h2').contains('Manage Departments').should('be.visible');
+  })
 });
 
-
-describe('Admin Page', () => {
-  it('Verify if the buttons show the data',() => {
-    cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/admin');
-    cy.contains('Show Employees').click();
-    cy.contains('Show Team Roles').click();
-    cy.contains('Manage Departments').click();
-  });
-});
-
-describe('Display Projects', () => {
+describe('Project Manger', () => {
   beforeEach(() => {
-    cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/project');
-  });
-
-  it('Should verify the display of "Create Project" element', () => {
-    cy.contains('Toggle Create Project').click();
-    cy.get('.create-project-section').should('be.visible');
-  });
-  it('Should verify the display of the project details', () => {
-    cy.contains('Toggle Project Details').click();
-    cy.get('.projects-grid').should('be.visible');
-  });
-});
-
-
-describe('Create Project with Valid Data', () => {
-  beforeEach(() => {
-    cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/project'); 
-    cy.contains('Toggle Create Project').click();
+  cy.viewport(1920, 1080)
+    cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/project')
   });
   it('Should create a project with valid data', () => {
+    cy.xpath("//button[normalize-space()='Toggle Create Project']").click()
     cy.get('#name').type('Project Name');
     cy.get('#prPeriod').select('Fixed');
     cy.get('#startDate').type('2024-03-16');
@@ -159,6 +186,21 @@ describe('Create Project with Valid Data', () => {
     cy.get('input[type="text"][placeholder="Experience"]').type('Experience Value');
     cy.get('button').contains('Create Project').click();
  });
+  it('Should view the project details', () => {
+    cy.xpath("//button[normalize-space()='Toggle Project Details']").click().should('be.visible')
+  });
+  it('Should make the Assignment Proposal be visible',()=>{
+    cy.xpath("//button[normalize-space()='Toggle Assignment Proposals']").click().should('be.visible')
+  })
 });
-
-
+describe('Employee Sign-Up',()=>{
+  it('Verify the registration of a new employee',()=>{
+    cy.viewport(1920, 1080);
+    cy.visit('https://atc-2024-code-218-fe-linux-web-app.azurewebsites.net/employee-register');
+    cy.get('[placeholder="Name"]').type("Nume ");
+    cy.get('[placeholder="Email"]').type('test@example.com'); // Change email as per your requirement
+    cy.get('[placeholder="Password"]').type('password123');
+    cy.xpath("//button[normalize-space()='Register']").click()
+    cy.xpath("//button[normalize-space()='Go to Login Page']").click()
+  })
+})
